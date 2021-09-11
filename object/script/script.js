81c20208aa9f9a6543feb32496d64a76c7f946b6
Fuaad <hydfwaad@gmail.com>
2021-09-11 19:10:05 +0200

    js oop completed

--- object/script/script.js
+++ object/script/script.js
@@ -1,22 +1,143 @@
-/* conhstructors */
+// console.log("hello i am %s string", "")
 
-function Phone(serial, color, price, showserial) {
-    this.serial = serial;
-    this.color = color;
-    this.price = price -100;
-    this.showserialf = function (){
-        if (showserial) {
-            return serial
-        }else{
-            return (`data is private`)
-        }
-    }
-}
-let phone1 = new Phone(121, "silverBlue", 500, true);
-let phone2 = new Phone(122, "Blue", 500, false);
-let phone3 = new Phone(123, "silver", 500, true);
+console.log(document)
+console.dir(document)
 
-//console.log(phone1);
+let div = document.querySelector(".cl");
+console.dir(div)
+div.addEventListener("click", () => {
+    div.style.color = "green"
+    setTimeout(() => {
+        div.style.fontSize = "55px"
+    }, 2000)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// class User {
+//     constructor(name, email) {
+//         this.name = name
+//         this.email = email
+//     }
+//     sayhello() {
+//         return `hello ${this.name}`
+//     }
+// }
+// let user1 = new User("fuaad")
+
+// console.log(user1.sayhello())
+
+
+// class Admin extends User {
+//     constructor(name, email) {
+//         super(name, email)
+//     }
+// }
+
+// let admin1 = new Admin("FuaadAdmin")
+
+// console.log(admin1.sayhello())
+
+
+
+
+
+
+
+
+
+
+//console.log(Document.prototype)
+
+// Window.prototype.$ = (e) => {
+//     this.document.querySelector(e)
+// }
+
+// let cl = window.$(".cl");
+
+// console.log(cl)
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Document.prototype)
+// // console.log(Document.$o())
+// // console.log(typeof Document)
+// Document.prototype.$ = function (e) {
+//     console.log(this)
+//     this.querySelector(e)
+// }
+// let cl = document.$(".cl")
+// console.log(cl)
+
+
+
+
+
+
+// console.log(String.prototype);
+
+
+// String.prototype.zFill = function (width) {
+//     let theRisult = this
+
+//     while(theRisult.length < width) {
+//         theRisult = `0${theRisult}`
+//     }
+//     return theRisult.toString()
+// }
+// console.log("12".zFill(5))
+// console.log("122".zFill(5))
+// console.log("1244".zFill(5))
+// console.log("12779".zFill(5))
+
+
+// String.prototype.sayYouLoveMe =function () {
+//     return `I love you ${this}`
+// }
+// console.log("Fuaad".sayYouLoveMe())
+
+
+
+
+
+
+// /* constructors */
+
+// function Phone(serial, color, price, showserial) {
+//     this.serial = serial;
+//     this.color = color;
+//     this.price = price -100;
+//     this.showserialf = function (){
+//         if (showserial) {
+//             return serial
+//         }else{
+//             return (`data is private`)
+//         }
+//     }
+// }
+// let phone1 = new Phone(121, "silverBlue", 500, true);
+// let phone2 = new Phone(122, "Blue", 500, false);
+// let phone3 = new Phone(123, "silver", 500, true);
+
+// //console.log(phone1);
 
 
 
